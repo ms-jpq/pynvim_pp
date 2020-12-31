777137fa3442da7658c7e5c4d612abcce6ba8cc3
Fly me to the moon <git@bigly.dog>
2020-12-31 04:39:42 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages(exclude=("tests*",))
+package_data = {pkg: ("py.typed",) for pkg in packages}
+
+setup(
+    name="pynvim2",
+    version="0.1.0",
+    description="Pynvim++",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/pynvim_pp",
+    packages=packages,
+    package_data=package_data,
+)
