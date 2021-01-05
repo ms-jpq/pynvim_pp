320caeb24b899fc8e4822ea515f714d18a8dc1ac
Fly me to the moon <git@bigly.dog>
2021-01-05 22:49:43 +0700

    thread safe

--- pynvim_pp/client.py
+++ pynvim_pp/client.py
@@ -35,7 +35,7 @@ class BasicClient(Client):
         handler = self._handlers.get(name, nil_handler(name))
         ret = handler(nvim, *args)
         if isinstance(ret, Awaitable):
-            self._q.put_nowait((name, args, ret))
+            run_coroutine_threadsafe(self._q.put((name, args, ret)))
             return None
         else:
             return ret
