b1388bb7403fada699e62b96db5a5bc07611df41
Fly me to the moon <git@bigly.dog>
2021-05-15 01:41:04 -0700

    typo

--- pynvim_pp/lib.py
+++ pynvim_pp/lib.py
@@ -88,11 +88,11 @@ def awrite(
 
 @contextmanager
 def bench(
-    nvim: Nvim, *args: Any, threshhold: float = 0.01, precision: int = 3
+    nvim: Nvim, *args: Any, threshold: float = 0.01, precision: int = 3
 ) -> Iterator[None]:
     t1 = monotonic()
     yield None
     t2 = monotonic()
     elapsed = t2 - t1
-    if elapsed >= threshhold:
+    if elapsed >= threshold:
         write(nvim, *args, round(elapsed, precision))
