997b9c49b800798ee46887896d52d03588261bce
IIIIII <git@bigly.dog>
2022-09-10 02:09:25 +0000

    off by 1

--- pynvim_pp/buffer.py
+++ pynvim_pp/buffer.py
@@ -45,7 +45,7 @@ class Buffer(Ext, HasLocalCall):
 
     @classmethod
     def from_int(cls, num: int) -> Buffer:
-        length = ceil(log(num) / log(256))
+        length = ceil(log(num) / log(255))
         return Buffer(data=num.to_bytes(length, byteorder="big"))
 
     @classmethod
